c988594c9b71e9b96a97666e5038fc5ff5a2f9bb
Henry Tito <140865436+HenryTito@users.noreply.github.com>
2025-10-21 19:20:48 -0300

    feat(KER-72): Criação páginas de login para Gestor, Funcionário e Master,
    Adição das páginas.jsx de login e arquivos .css para diferentes módulos.

--- views/frontend-app/components/layout/ConditionalLateralBar.jsx
+++ views/frontend-app/components/layout/ConditionalLateralBar.jsx
@@ -5,6 +5,11 @@ import LateralBar from './LateralBar/LateralBar';
 
 export default function ConditionalLateralBar() {
   const pathname = usePathname();
-  if (pathname === '/agendamento') return null;
+  if (pathname === '/formulario-agregado') return null;
+  if (pathname === '/pagina-agregado') return null;
+  if (pathname === '/checklist-veiculo') return null;
+  if(pathname ==='/login-funcionario') return null;
+  if(pathname ==='/login-gestor') return null;
+  if(pathname ==='/login-master') return null;
   return <LateralBar />;
 }
