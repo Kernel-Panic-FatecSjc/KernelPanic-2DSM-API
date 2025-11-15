f4f9d835fea31a9e904493e5fae6719c0682823c
Vinícius Leite <vinicius.leite17@fatec.sp.gov.br>
2025-11-14 23:33:18 -0300

    feat(KER-134): Calendário estilizado e funcional, tendo tudo que foi pedido no figma na parte de design,
    Dias que tem eventos em azul claro, e o dia de hoje em azul escuro

--- views/frontend-app/app/eventos/page.jsx
+++ views/frontend-app/app/eventos/page.jsx
@@ -6,14 +6,21 @@ import "react-day-picker/dist/style.css";
 import styles from "./App.module.css";
 
 export default function Calendario() {
-  const [selected, setSelected] = useState();
-  const [month, setMes] = useState(new Date());
+  function Data(ano, mes, dia) {
+  return new Date(ano, mes - 1, dia);
+  }
 
   const eventos = [
-  { id: 1, date: new Date(2025, 9, 27), nome: "Evento X" },
-  { id: 2, date: new Date(2025, 9, 30), nome: "Evento Y" }
+    { id: 1, date: new Data(2025, 11, 27), nome: "Evento X" },
+    { id: 2, date: new Data(2025, 11, 30), nome: "Evento Y" }
   ];
 
+  const [selected, setSelected] = useState([]); 
+
+  const [month, setMes] = useState(new Date());
+  const weekdays = ["Dom", "Seg", "Ter", "Qua", "Qui", "Sex", "Sáb"];
+
+  console.log("Selecionados agora:", selected);
 
   function handlePrevious() {
     setMes(new Date(month.setMonth(month.getMonth() - 1)));
@@ -26,21 +33,14 @@ export default function Calendario() {
   return (
     <div className={styles.container}>
       <div className={styles.calendarContainer}>
+
         <div className={styles.navCustom}>
           <button className={styles.calendarButton} onClick={handlePrevious}>
-            <img
-              src="images/setinha-esquerda.svg"
-              alt="previous"
-              style={{ color: "white" }}
-            />
+            <img src="images/setinha-esquerda.svg" alt="previous" />
           </button>
 
           <button className={styles.calendarButton} onClick={handleNext}>
-            <img
-              src="images/setinha.svg"
-              alt="next"
-              style={{ color: "white" }}
-            />
+            <img src="images/setinha.svg" alt="next" />
           </button>
 
           <h2 className={styles.calendarTitle}>
@@ -52,18 +52,37 @@ export default function Calendario() {
         </div>
 
         <DayPicker
-          mode="single"
+          mode="multiple"
           selected={selected}
-          onSelect={setSelected}
+          onSelect={(days) => {
+            console.log("Dias clicados:", days);
+            setSelected(days ?? []);
+          }}
           month={month}
           onMonthChange={setMes}
+          modifiers={{
+            hoje: new Date(),
+            eventos: eventos.map((e) => e.date),
+          }}
+
+          modifiersClassNames={{
+            hoje: styles.hoje,
+            eventos: styles.evento,
+          }}
+
+          formatters={{
+            formatWeekdayName: (weekday) => weekdays[weekday.getDay()],
+          }}
+
           components={{
             Caption: () => null,
             CaptionLabel: () => null,
             Nav: () => null,
           }}
         />
+
       </div>
+
       <div className={styles.eventosContainer}>
         <h2 className={styles.eventosTitle}>Eventos</h2>
       </div>
